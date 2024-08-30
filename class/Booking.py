c164bf5f2e23f2266010e78f986ab3649cade071
SuzenAkhtar <136620016+SuzenAkhtar@users.noreply.github.com>
2024-08-30 11:42:01 +0530

    Create Booking.py

---
+++ class/Booking.py
@@ -0,0 +1,11 @@
+class Booking:
+    def __init__(self, pnr_no, name, full_name, age, date_of_journey, train, train_class, from_station, to_station):
+        self.pnr_no = pnr_no
+        self.name = name
+        self.full_name = full_name
+        self.age = age
+        self.date_of_journey = date_of_journey
+        self.train = train
+        self.train_class = train_class
+        self.from_station = from_station
+        self.to_station = to_station
