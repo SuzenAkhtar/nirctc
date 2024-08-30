e6e25608aca2b60d2d7ef3bc8e411150573ea6a6
SuzenAkhtar <136620016+SuzenAkhtar@users.noreply.github.com>
2024-08-30 11:46:03 +0530

    Create Train.py

---
+++ class/Train.py
@@ -0,0 +1,6 @@
+class Train:
+    def __init__(self, name, source, destination, no_of_seats):
+        self.name = name
+        self.source = source
+        self.destination = destination
+        self.no_of_seats = no_of_seats
